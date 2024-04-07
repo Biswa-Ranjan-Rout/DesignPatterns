7a4a6d83e169e9bb340b5cf3273e673723ebfdc3
Biswa-Ranjan-Rout <90982608+Biswa-Ranjan-Rout@users.noreply.github.com>
2024-04-08 00:18:21 +0530

    Update Client.java

--- BehaviouralPatterns/AdapterDesignPattern/com/adp/Client.java
+++ BehaviouralPatterns/AdapterDesignPattern/com/adp/Client.java
@@ -1,13 +1,15 @@
 package com.adp;
 
+// Step-4
 public class Client {
-	public static void main(String[] args) {
-		showTemprature(759001);
-	}
-
+	
 	private static void showTemprature(int pin) {
 		WeatherAdapter adapter = new WeatherAdapter();
 		int temp = adapter.findTemprature(pin);
 		System.out.println("Temprature: " + temp);
+	}
+
+	public static void main(String[] args) {
+		showTemprature(759001);
 	}
 }
